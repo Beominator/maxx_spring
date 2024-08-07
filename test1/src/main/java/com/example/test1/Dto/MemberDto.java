6576ea70697d706da3adc118d19d5608af662cee
Beominator <woqja87@gmail.com>
2024-08-07 09:07:33 +0900

    0806

---
+++ test1/src/main/java/com/example/test1/Dto/MemberDto.java
@@ -0,0 +1,36 @@
+package com.example.test1.Dto;
+
+public class MemberDto {
+	private String id;
+	private String pw;
+	private String tel;
+	private String birth;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
+}
---
+++ test1/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,19 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1> 나는 login</h1>
+	
+	<form method="post" action="/login">
+			아이디 : <input type="text" name="id"> <br>
+			비밀번호 : <input type="password" name="pw"> <br>
+			<button> 로그인 </button>
+	</form>	
+</body>
+</html>
